46f62d759de5ceb475af81bce599497afa9a74da
Nikhil Singh <nikhilbhadauriya.gspt@gmail.com>
2026-04-23 01:17:24 +0530

    ss

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,11 @@ export default function Hero() {
   return (
     <div className="w-full bg-white font-['Rubik'] overflow-hidden">
       <div className="w-full h-[250px] md:h-[450px] lg:h-[600px] xl:h-[800px] relative">
-        <Link to="/shop" className="block w-full h-full cursor-pointer">
-          <img
-            src="/banner/new-banner/2.jpg"
-            alt="Premium Printer Banner"
-            className="w-full h-full object-cover select-none"
-          />
-        </Link>
+        <img
+          src="/banner/new-banner/2.jpg"
+          alt="Premium Printer Banner"
+          className="w-full h-full object-cover select-none"
+        />
       </div>
     </div>
   );
